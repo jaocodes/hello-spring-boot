016d0c64ffc7810fc6abe07f40f6e71eb5b1a479
jaocodes <jhonskaz@gmail.com>
2025-11-16 22:43:57 -0300

    project setup

---
+++ src/main/java/com/jotace/first_project_spring_boot/FirstProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jotace.first_project_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jotace/first_project_spring_boot/FirstProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotace.first_project_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
