37aaa1257c3bbb13312fea8efcfaa83ceb1a6e27
jaocodes <jhonskaz@gmail.com>
2025-11-16 23:23:08 -0300

    creating the first endpoint

---
+++ src/main/java/com/jotace/first_project_spring_boot/Controller.java
@@ -0,0 +1,14 @@
+package com.jotace.first_project_spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/firstController")
+public class Controller {
+    @GetMapping("/firstEndpoint")
+    public String firstEndpoint(){
+        return  "Minha primeira rota API REST";
+    }
+}
