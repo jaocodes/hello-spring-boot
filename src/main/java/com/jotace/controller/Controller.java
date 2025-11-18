bd573d26062cf5ab13ac285cd767bcc0b31cb834
jaocodes <jhonskaz@gmail.com>
2025-11-17 22:03:58 -0300

    using path params on endpoints

--- src/main/java/com/jotace/controller/Controller.java
+++ src/main/java/com/jotace/controller/Controller.java
@@ -1,14 +1,15 @@
 package com.jotace.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/firstController")
 public class Controller {
-    @GetMapping("/firstEndpoint")
-    public String firstEndpoint(){
-        return  "Minha primeira rota API REST";
+    @GetMapping("/firstEndpoint/{id}")
+    public String firstEndpoint(@PathVariable String id){
+        return  "O parâmetro é: " + id;
     }
 }
