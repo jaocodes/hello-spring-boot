0122aeb62eeab93c777129ae1fdad500e8000e01
jaocodes <jhonskaz@gmail.com>
2025-11-17 21:59:50 -0300

    using ComponentScan annotation

--- src/main/java/com/jotace/controller/Controller.java
+++ src/main/java/com/jotace/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.jotace.first_project_spring_boot;
+package com.jotace.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/jotace/first_project_spring_boot/FirstProjectSpringBootApplication.java
+++ src/main/java/com/jotace/first_project_spring_boot/FirstProjectSpringBootApplication.java
@@ -1,9 +1,14 @@
 package com.jotace.first_project_spring_boot;
 
+import com.jotace.controller.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages =  "com.jotace")
+@ComponentScan(basePackageClasses =  Controller.class)
+
 public class FirstProjectSpringBootApplication {
 
 	public static void main(String[] args) {
